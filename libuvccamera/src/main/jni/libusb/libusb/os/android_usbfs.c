1e5c2061561ddefdb36744c594a25742f8ed978b
lgon528 <lgon528@gmail.com>
2019-03-24 19:52:21 +0800

    fix nullptr crash

--- libuvccamera/src/main/jni/libusb/libusb/os/android_usbfs.c
+++ libuvccamera/src/main/jni/libusb/libusb/os/android_usbfs.c
@@ -2726,9 +2726,9 @@ static int handle_iso_completion(struct libusb_device_handle *handle,	// XXX add
 
 	usbi_mutex_lock(&itransfer->lock);
 	for (i = 0; i < num_urbs; i++) {
-		if (urb == tpriv->iso_urbs[i]) {
-			urb_idx = i + 1;
-			break;
+	    if (tpriv->iso_urbs && urb == tpriv->iso_urbs[i]) {
+            urb_idx = i + 1;
+            break;
 		}
 	}
 	if (UNLIKELY(urb_idx == 0)) {
